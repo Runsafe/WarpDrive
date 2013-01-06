88fe6e901bbe1e96129acd731cb7a92ebb940402
Morten Nilsen <morten@runsafe.no>
2013-01-06 16:36:44 +0100

    Refactor and optimize snazzy warps, dont use bukkit api async

--- src/no/runsafe/warpdrive/SnazzyWarp.java
+++ src/no/runsafe/warpdrive/SnazzyWarp.java
@@ -1,60 +1,126 @@
 package no.runsafe.warpdrive;
 
+import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IAsyncEvent;
+import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.event.player.IPlayerRightClickSign;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeLocation;
+import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.block.RunsafeSign;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
+import no.runsafe.framework.timer.ForegroundWorker;
+import no.runsafe.framework.timer.IScheduler;
 import org.bukkit.ChatColor;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
 
-import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
 
-import static no.runsafe.warpdrive.StaticWarp.safePlayerTeleport;
+import static no.runsafe.warpdrive.StaticWarp.findSafeSpot;
+import static no.runsafe.warpdrive.StaticWarp.targetFloorIsSafe;
 
-public class SnazzyWarp implements IPlayerRightClickSign, IAsyncEvent
+public class SnazzyWarp extends ForegroundWorker<String, SnazzyWarp.WarpParameters> implements IPlayerRightClickSign, IAsyncEvent, IConfigurationChanged
 {
+	public SnazzyWarp(IScheduler scheduler, IOutput output)
+	{
+		super(scheduler, 2);
+		console = output;
+	}
+
 	@Override
 	public boolean OnPlayerRightClickSign(RunsafePlayer thePlayer, RunsafeItemStack runsafeItemStack, RunsafeSign theSign)
 	{
 		if (theSign.getLine(0).equalsIgnoreCase(ChatColor.DARK_BLUE + "[Snazzy Warp]"))
 		{
-			safePlayerTeleport(getRandomWarp(theSign), thePlayer, true);
+			if (!snazzyWarps.containsKey(theSign.getLine(1)))
+				snazzyWarps.put(theSign.getLine(1), new WarpParameters(theSign));
+			Push(thePlayer.getName(), snazzyWarps.get(theSign.getLine(1)));
+			console.outputDebugToConsole("Pushed player click..", Level.FINE);
 			return false;
 		}
 		return true;
 	}
 
-	private RunsafeLocation getRandomWarp(RunsafeSign theSign)
+	@Override
+	public void process(String player, WarpParameters parameters)
 	{
-		String warpName = theSign.getLine(1);
-
-		if (this.signWarpExpires.containsKey(warpName))
-		{
-			long age = (System.currentTimeMillis() - this.signWarpExpires.get(warpName)) / 1000;
-			if (age <= 60)
-				return signWarpLocations.get(warpName);
-		}
-		int radius = Integer.parseInt(theSign.getLine(2));
-		int boundingRadius = Integer.parseInt(theSign.getLine(3));
-
-		boolean negX = Math.random() * 100.0D > 50;
-		boolean negZ = Math.random() * 100.0D > 50;
-
-		double randomXB = (negX ? -1 : 1) * ((radius - boundingRadius) * Math.random() + boundingRadius);
-		double randomZB = (negZ ? -1 : 1) * ((radius - boundingRadius) * Math.random() + boundingRadius);
-
-		double randomX = randomXB + theSign.getX();
-		double randomZ = randomZB + theSign.getZ();
-
-		RunsafeLocation newLocation = new RunsafeLocation(theSign.getWorld(), randomX, 60.0D, randomZ);
-
-		signWarpLocations.put(warpName, newLocation);
-		signWarpExpires.put(warpName, System.currentTimeMillis());
-
-		return signWarpLocations.get(warpName);
+		console.outputDebugToConsole(String.format("Player %s teleporting", player), Level.FINE);
+		RunsafePlayer target = RunsafeServer.Instance.getPlayer(player);
+		RunsafeLocation destination = parameters.getTarget();
+		if (target.isOnline() && destination != null)
+			target.teleport(destination);
+		else
+			console.outputDebugToConsole("Unable to find destination..", Level.FINE);
 	}
 
-	private final HashMap<String, RunsafeLocation> signWarpLocations = new HashMap<String, RunsafeLocation>();
-	private final HashMap<String, Long> signWarpExpires = new HashMap<String, Long>();
+	@Override
+	public void OnConfigurationChanged(IConfiguration configuration)
+	{
+		change_after = Duration.standardSeconds(configuration.getConfigValueAsInt("snazzy.timeout"));
+	}
+
+	class WarpParameters
+	{
+		WarpParameters(RunsafeSign sign)
+		{
+			maxDistance = Integer.parseInt(sign.getLine(2));
+			minDistance = Integer.parseInt(sign.getLine(3));
+			world = sign.getWorld();
+			originX = sign.getX();
+			originZ = sign.getZ();
+			console.outputDebugToConsole(
+				String.format(
+					"Configured new warp sign %s in world %s [%d-%d] at (%d,%d).",
+					sign.getLine(1),
+					world.getName(),
+					minDistance,
+					maxDistance,
+					originX,
+					originZ
+				),
+				Level.FINE
+			);
+		}
+
+		public RunsafeLocation getTarget()
+		{
+			if (target == null || expires.isBefore(DateTime.now()) || !targetFloorIsSafe(target, true))
+			{
+				target = getNewTarget();
+				expires = DateTime.now().plus(change_after);
+			}
+			return target;
+		}
+
+		private RunsafeLocation getNewTarget()
+		{
+			boolean negX = rng.nextInt(100) > 50;
+			boolean negZ = rng.nextInt(100) > 50;
+			double randomX = (negX ? -1 : 1) * (rng.nextInt(maxDistance - minDistance) + minDistance);
+			double randomZ = (negZ ? -1 : 1) * (rng.nextInt(maxDistance - minDistance) + minDistance);
+			RunsafeLocation target = null;
+			int retries = 10;
+			while (target == null && retries-- > 0)
+				target = findSafeSpot(new RunsafeLocation(world, randomX + originX + 0.5, 64.0D, randomZ + originZ + 0.5), true);
+			return target;
+		}
+
+		private final RunsafeWorld world;
+		private final int originX;
+		private final int originZ;
+		private final int maxDistance;
+		private final int minDistance;
+		private final Random rng = new Random();
+		private DateTime expires = DateTime.now();
+		private RunsafeLocation target;
+	}
+
+	private final ConcurrentHashMap<String, WarpParameters> snazzyWarps = new ConcurrentHashMap<String, WarpParameters>();
+	private final IOutput console;
+	private Duration change_after;
 }
--- src/no/runsafe/warpdrive/StaticWarp.java
+++ src/no/runsafe/warpdrive/StaticWarp.java
@@ -4,12 +4,25 @@ import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.player.RunsafePlayer;
+import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
 
 public class StaticWarp
 {
 	public static boolean safePlayerTeleport(RunsafeLocation originalLocation, RunsafePlayer player, boolean forceTop)
+	{
+		RunsafeLocation target = findSafeSpot(originalLocation, forceTop);
+		if (target != null)
+		{
+			player.setFallDistance(0.0F);
+			player.teleport(target);
+			return true;
+		}
+		return false;
+	}
+
+	public static RunsafeLocation findSafeSpot(RunsafeLocation originalLocation, boolean forceTop)
 	{
 		int maxScan = 100;
 		double x = originalLocation.getX();
@@ -22,22 +35,21 @@ public class StaticWarp
 			originalLocation.getYaw(),
 			originalLocation.getPitch()
 		);
-		if (forceTop || !targetIsSafe(location))
+		if (forceTop || !targetFloorIsSafe(location, false))
 			location = findTop(location);
 
 		while (true)
 		{
-			if (targetIsSafe(location))
+			if (targetFloorIsSafe(location, false))
 			{
 				location.setY(location.getY() + 1);
-				player.setFallDistance(0.0F);
-				player.teleport(location);
-				return true;
+				return location;
 			}
 			maxScan--;
 			if (maxScan < 0)
-				return false;
+				return null;
 
+			// Spiral out to find a safe location
 			double t = 100 - maxScan;
 			location.setX(5 * t * Math.cos(t) + x);
 			location.setZ(5 * t * Math.sin(t) + z);
@@ -48,33 +60,43 @@ public class StaticWarp
 	public static RunsafeLocation findTop(RunsafeLocation location)
 	{
 		RunsafeWorld world = location.getWorld();
-		int maxHeight = world.getMaxHeight();
-		int minHeight = 1;
-
 		if (location.getWorld().getRaw().getEnvironment() == World.Environment.NETHER)
 		{
-			maxHeight = 126;
-			minHeight = 0;
+			int maxHeight = 125;
+			int minHeight = 4;
+			int y = maxHeight - 1;
+			int air = 0;
+			while (y > minHeight)
+			{
+				location.setY(y);
+				if (world.getBlockAt(location).isAir())
+					air++;
+				else
+					air = 0;
+				if (air > 1)
+					break;
+				y--;
+			}
+			return location;
 		}
-
-		int y = maxHeight - 1;
-		while (y > minHeight)
-		{
-			location.setY(y);
-			if (!world.getBlockAt(location).isAir()) //location.getBlockX(), y, location.getBlockZ()).isAir())
-				break;
-			y--;
-		}
-		return location;
+		else
+			return new RunsafeLocation(world.getRaw().getHighestBlockAt(location.getRaw()).getLocation());
 	}
 
-	public static boolean targetIsSafe(RunsafeLocation location)
+	public static boolean targetFloorIsSafe(RunsafeLocation location, boolean playerLocation)
 	{
-		RunsafeBlock floor = location.getWorld().getBlockAt(location);
-		if (floor.canPassThrough() && floor.getTypeId() != Material.WATER.getId())
+		Chunk chunk = location.getWorld().getRaw().getChunkAt(location.getRaw());
+		if (!chunk.isLoaded())
+			chunk.load();
+		RunsafeBlock floor;
+		if (playerLocation)
+			floor = location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ());
+		else
+			floor = location.getWorld().getBlockAt(location);
+		if (floor.isHazardous() || (floor.canPassThrough() && floor.getTypeId() != Material.WATER.getId()))
 			return false;
 
-		for (int y = 1; y < 3; ++y)
+		for (int y = playerLocation ? 0 : 1; y < (playerLocation ? 2 : 3); ++y)
 		{
 			RunsafeBlock block = location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() + y, location.getBlockZ());
 			if (block.isHazardous() || !block.canPassThrough())
