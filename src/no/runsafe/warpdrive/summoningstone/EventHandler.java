22facc7d7eeae48cc84f5483eedcb97647b78ceb
Morten Nilsen <morten@runsafe.no>
2013-06-09 16:09:22 +0200

    Fix API Breakage

--- src/no/runsafe/warpdrive/summoningstone/EventHandler.java
+++ src/no/runsafe/warpdrive/summoningstone/EventHandler.java
@@ -14,6 +14,7 @@ import no.runsafe.framework.server.event.player.RunsafePlayerJoinEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerPortalEvent;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.item.meta.RunsafeBook;
+import no.runsafe.framework.server.item.meta.RunsafeMeta;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Effect;
 import org.bukkit.Material;
@@ -70,7 +71,7 @@ public class EventHandler implements IPlayerPortalEvent, IEntityPortalEnterEvent
 	}
 
 	@Override
-	public boolean OnPlayerRightClick(RunsafePlayer runsafePlayer, RunsafeItemStack itemStack, RunsafeBlock runsafeBlock)
+	public boolean OnPlayerRightClick(RunsafePlayer runsafePlayer, RunsafeMeta itemStack, RunsafeBlock runsafeBlock)
 	{
 		if (itemStack == null)
 			return true;
